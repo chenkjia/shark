ffd4f8eed22813ac99de51c2e6d79786692bf465
可嘉 陈 <chenkjia@live.com>
2020-09-05 21:54:34 +0800

    时间调整

--- app/service/ok.js
+++ app/service/ok.js
@@ -1,24 +1,12 @@
 'use strict';
-const Service = require('egg').Service;
+const Service = require('./common');
 // moment().utcOffset();
 class OkService extends Service {
   async trade() {
-    const timestamp = this.ctx.service.utils.getTime();
-    const callback = await this.ctx.curl('http://okex.me/api/general/v3/time', {
-      followRedirect: true,
-      contentType: 'json',
-      dataType: 'json',
-    });
+    const callback = await this.request({
+      url: `/api/general/v3/time`
+    })
     console.log(callback.data.iso)
-    // const callback = await this.ctx.curl('http://api.waditu.com', {
-    //   method: 'POST',
-    //   contentType: 'json',
-    //   data: {
-    //     api_name: 'stock_basic',
-    //     token: '021aced2154517da19b6d76dca1f9fc02b8fdaa5aa1a8e43fee373fd',
-    //   },
-    //   dataType: 'json',
-    // });
     return callback.data;
   }
 }
