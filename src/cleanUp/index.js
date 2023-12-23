415c20d91b1093682d8f68ec8b2dce8812328d7c
可嘉 陈 <chenkjia@live.com>
2023-12-23 13:45:30 +0800

    fixbug

--- src/cleanUp/index.js
+++ src/cleanUp/index.js
@@ -1,10 +1,5 @@
 import { read, update } from '../libs/mongodbClient.js'
-import axios from "axios";
-//
-const getEigenphi = async (hash) => {
-  return await axios.get(`https://storage.googleapis.com/eigenphi-ethereum-tx/${hash}`)
-    .then(({data}) => data).catch(error => ({}))
-}
+import getEigenphi from './getEigenphi.js'
 
 // 定义清理数据机器人
 const cleanUpBot = () => {
@@ -20,7 +15,8 @@ const cleanUpBot = () => {
 // 定义清理数据函数
 export const cleanUpBatch = async () => {
   // 定时读取mongodb的transfer数据,需要过滤isCleanUp的数据
-  const waitForcleanUp = await read('eagle', 'transfers', { isCleanUp: { $ne: true } })
+  // const waitForcleanUp = await read('eagle', 'transfers', { isCleanUp: { $ne: true } })
+  const waitForcleanUp = await read('eagle', 'transfers')
   // 如果有数据未cleanUp,则对该数据执行cleanUp方法
   for (const item of waitForcleanUp) {
     await cleanUp(item);
@@ -30,7 +26,7 @@ const cleanUp = async (transfer) => {
   // 通过时间和blockNum确认其是否为过期数据，如果是，则将数据加一个isAbandon状态
 
   // 通过getEigenphi判断是否该交易已被夹，将是否被夹的信息保存进数据库
-  const eigenphi = await getEigenphi(transfer);
+  const eigenphi = await getEigenphi(transfer.hash);
   // sandWich:{transactionToAddress,gasPrice}
   // 最后为数据加上isCleanUp:true的状态
   let set = {}
---
+++ test/cleanUp/getEigenphi.js
@@ -0,0 +1,6 @@
+
+import getEigenphi from '../../src/cleanUp/getEigenphi.js'
+(async () => {
+  const a = await getEigenphi('0xeb1b907ff183281994b479e235057d6c678e50949021e760b6e241a16d310754')
+  console.log(a)
+})();
